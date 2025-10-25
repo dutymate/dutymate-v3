726fe496587c53c1f304d73b2a1fd379428702ad
MINSSUNGKIM <planman0@gmail.com>
2025-10-25 15:48:01 +0900

    [DUTYMATE-42] feat: Implement admin page backend api

--- backend/src/main/java/net/dutymate/api/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/net/dutymate/api/domain/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import net.dutymate.api.domain.member.Member;
@@ -18,4 +19,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Boolean existsByEmail(String email);
 
 	List<Member> findByEmailEndingWith(String suffix);
+
+	@Query("SELECT COUNT(m) FROM Member m WHERE m.email NOT LIKE '%tempEmail@temp.com%'")
+	long countRealUsers();
 }
